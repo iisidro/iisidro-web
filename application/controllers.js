e4120c859c3abaae52b23ef88fc7c16a845d8cf6
Tomas <tnovau@gmail.com>
2016-09-17 17:09:48 -0300

    [Tomas Novau] - controllers add

--- application/controllers.js
+++ application/controllers.js
@@ -7,4 +7,6 @@ module.exports.injectControllersTo = (mod) => {
     require('application/controllers/app/admin/questions/QuestionCreateEditController').injectControllerTo(mod);
     require('application/controllers/app/admin/surveys/SurveysListController').injectControllerTo(mod);
     require('application/controllers/app/admin/surveys/SurveyCreateEditController').injectControllerTo(mod);
+    require('application/controllers/app/admin/sections/SectionsListController').injectControllerTo(mod);
+    require('application/controllers/app/admin/sections/SectionsCreateEditController').injectControllerTo(mod);
 };
