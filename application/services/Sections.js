04c8f94ba9d8f118cbe7adee9a8e6cfe6414434b
Tomas <tnovau@gmail.com>
2016-09-17 16:29:28 -0300

    [Tomas Novau] - Vistas de secciones

---
+++ application/services/Sections.js
@@ -0,0 +1,69 @@
+module.exports.injectServiceTo = (mod) => {
+    mod.service('Sections', [
+        'API',
+        '$q',
+        function (API, $q) {
+
+            this.getInstances = () => {
+                return $q((resolve, reject) => {
+                    API.get({
+                        url: config.surveyId + '/secciones'
+                    })
+                    .then((response) => {
+                        resolve(response.data);
+                    })
+                    .catch((error) => {
+                        reject(error);
+                    });
+                });
+            };
+
+            this.createInstance = (config) => {
+                let section = config.section;
+
+                return $q((resolve, reject) => {
+                    API.post({
+                        url: 'secciones',
+                        data: {
+                            nombre: section.title
+                        }
+                    })
+                    .then((response) => {
+                        resolve(response);
+                    })
+                    .catch((error) => {
+                        reject(error);
+                    });
+                });
+            };
+
+            this.getInstance = (config = {}) => {
+                return $q((resolve, reject) =>{
+                    API.get({
+                        url: config.surveyId + '/secciones/' + config.sectionId
+                    })
+                    .then((response) =>{
+                        resolve(response);
+                    })
+                    .catch((error) =>{
+                        reject(error);
+                    });
+                });
+            }
+
+            this.deleteInstance = (config = {}) => {
+                return $q((resolve, reject) => {
+                    API.delete({
+                        url: config.surveyId + '/secciones/' + config.sectionId
+                    })
+                    .then((response) => {
+                        resolve(response);
+                    })
+                    .catch((error) => {
+                        reject(error);
+                    });
+                });
+            }
+        }
+    ]);
+};
