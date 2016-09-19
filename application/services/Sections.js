798be2c870b1af83a1e7ba0c17f74463b3492be7
tnovau <tnovau@gmail.com>
2016-09-19 17:30:21 -0300

    [Tomas Novau] - Sections Response Data 2

--- application/services/Sections.js
+++ application/services/Sections.js
@@ -29,7 +29,7 @@ module.exports.injectServiceTo = (mod) => {
                         }
                     })
                     .then((response) => {
-                        resolve(response);
+                        resolve(response.data);
                     })
                     .catch((error) => {
                         reject(error);
