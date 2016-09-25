e9e93cd93f9a22c3350e0601bb713bc51ae0895c
Tomas <tnovau@gmail.com>
2016-09-25 11:04:00 -0300

    [Tomas Novau] - Secciones

--- application/controllers/app/admin/surveys/SurveyCreateEditController.js
+++ application/controllers/app/admin/surveys/SurveyCreateEditController.js
@@ -3,7 +3,8 @@ module.exports.injectControllerTo = (mod) => {
         'Surveys',
         '$state',
         '$mdDialog',
-        function (Surveys, $state, $mdDialog) {
+        'Sections',
+        function (Surveys, $state, $mdDialog, Sections) {
 
             this.initialize = () => {
                 if ($state.params.hasOwnProperty('surveyId')) {
@@ -11,14 +12,24 @@ module.exports.injectControllerTo = (mod) => {
                         surveyId: $state.params.surveyId
                     })
                     .then((survey) => {
-                        console.log(survey);
+                        //console.log(survey);
                         this.survey.id = $state.params.surveyId;
                         this.survey.title = survey.nombre;
                     })
                     .catch((error) => {
                         console.log(error);
                     });
-                    console.log(this.survey);
+                    //console.log(this.survey);
+
+                    Sections.getInstances({
+                        surveyId: $state.params.surveyId
+                    })
+                    .then((sections) => {
+                        this.sections = sections;
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
                 }
             };
 
@@ -90,12 +101,44 @@ module.exports.injectControllerTo = (mod) => {
                 $state.go('base.app.admin.surveys.list');
             };
 
+            this.deleteSection = () => {
+
+            };
+
+            this.editSection = () => {
+
+            }
+
             this.survey = {
                 id: -1,
                 title: ''
             };
 
-            this.texto = '';
+            this.sections = [];
+            this.columns = [
+                {
+                    key: 'orden',
+                    label: 'Orden'
+                },
+                {
+                    key: 'codigo',
+                    label: 'Código'
+                },
+                {
+                    key: 'nombre',
+                    label: 'Nombre'
+                }
+            ];
+            this.actions = [
+                {
+                    label: 'Editar',
+                    onCall: this.editSection
+                },
+                {
+                    label: 'Eliminar',
+                    onCall: this.deleteSection
+                }
+            ];
         }
     ]);
 };
